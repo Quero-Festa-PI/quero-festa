88273bc70269fecaf0f1e26b19d9d4db9c0df9eb
Pedro Griff Marcincowski <marcincowski@gmail.com>
2020-04-02 21:21:47 -0300

    Adicionando rotas para as páginas
    Incluídas rotas para as páginas Aniversário, Bodas, Datas Especiais e Comemorações.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 router.get('/', function(req, res, next) {
   res.render('index', {page:'Home', menuId:'home'});
+});
+
+router.get('/aniversario', function(req, res, next) {
+  res.render('aniversario', {page:'Aniversario', menuId:'aniversario'});
+});
+
+router.get('/bodas', function(req, res, next) {
+  res.render('bodas', {page:'Bodas', menuId:'bodas'});
+});
+
+router.get('/especiais', function(req, res, next) {
+  res.render('especiais', {page:'Especiais', menuId:'especiais'});
+});
+
+router.get('/comemoracoes', function(req, res, next) {
+  res.render('comemoracoes', {page:'comemoracoes', menuId:'comemoracoes'});
 });
 
 module.exports = router;
