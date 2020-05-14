0e7e375a7a9b4659fe64b2532e2c975fefb9760a
navarroleonardo <abreunavarro.leonardo@gmail.com>
2020-05-14 16:16:18 -0300

    criado model Usuario

---
+++ models/Usuario.js
@@ -0,0 +1,52 @@
+let Usuario = (sequelize, DataTypes) => {
+    let usuario = sequelize.define(
+        'Usuario',
+        {
+            id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true
+            },
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            sobrenome: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true
+            },
+            senha: {
+                type: DataTypes.CHAR(256),
+                allowNull: false
+            },
+            cpf: {
+                type: DataTypes.CHAR(11),
+                allowNull: false,
+                unique: true
+            },
+            data_nasc: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            sexo: {
+                type: DataTypes.ENUM('M', 'F'),
+                allowNull: false
+            },
+        },
+        {
+            tableName: 'usuarios',
+            timestamps: false
+        }
+    )
+
+    return usuario;
+
+}
+
+module.exports = Usuario;
