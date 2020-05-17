5c3f46b235ac00c2df089cdbf32d6f299b1d6f0d
navarroleonardo <abreunavarro.leonardo@gmail.com>
2020-05-17 16:32:26 -0300

    adicionado rotas post e controllers para cadastro e login, e feito ajustes  nas respectivas views

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,25 @@ var router = express.Router();
 
 // Chamando os controllers 
 const lojaController = require('../controllers/lojaController');
-const produtoController = require ('../controllers/produtoController');
+const produtoController = require('../controllers/produtoController');
 
 //Requisições loja
-router.get ('/', lojaController.index);
-router.get ('/logar', lojaController.logar);
-router.get ('/cadastro', lojaController.cadastro);
-router.get ('/dashboard', lojaController.dashboard);
-router.get ('/painel-cliente', lojaController.painelCliente);
-router.get ('/cliente-cadastro', lojaController.clienteCadastro);
-router.get ('/loja', lojaController.loja);
+router.get('/', lojaController.index);
+router.get('/logar', lojaController.login);
+router.post('/logar', lojaController.logar);
+router.get('/cadastro', lojaController.cadastro);
+router.post('/cadastro', lojaController.cadastrar);
+router.get('/dashboard', lojaController.dashboard);
+router.get('/painel-cliente', lojaController.painelCliente);
+router.get('/cliente-cadastro', lojaController.clienteCadastro);
+router.get('/loja', lojaController.loja);
 
 // Requisições produtos
-router.get ('/buscar' , produtoController.buscar);
-router.get ('/cadastrar-produto', produtoController.cadastrar);
+router.get('/buscar', produtoController.buscar);
+router.get('/cadastrar-produto', produtoController.cadastrar);
 
 //Categorias
-router.get ('/aniversario', produtoController.aniversario);
+router.get('/aniversario', produtoController.aniversario);
 router.get('/bodas', produtoController.bodas);
 router.get('/especiais', produtoController.especiais);
 router.get('/comemoracoes', produtoController.comemoracoes);
@@ -30,25 +32,25 @@ router.get('/comemoracoes', produtoController.comemoracoes);
 router.get('/perfil-vendedor', lojaController.perfilVendedor);
 
 
-router.get('/cart', function(req, res, next) {
-  res.render('cart', {page:'cart', menuId:'cart'});
+router.get('/cart', function (req, res, next) {
+  res.render('cart', { page: 'cart', menuId: 'cart' });
 });
-router.get('/checkout', function(req, res, next) {
-  res.render('checkout', {page:'checkout', menuId:'checkout'});
+router.get('/checkout', function (req, res, next) {
+  res.render('checkout', { page: 'checkout', menuId: 'checkout' });
 });
-router.get('/confirmacao', function(req, res, next) {
-  res.render('confirmacao', {page:'confirmacao', menuId:'confirmacao'});
+router.get('/confirmacao', function (req, res, next) {
+  res.render('confirmacao', { page: 'confirmacao', menuId: 'confirmacao' });
 });
 
-
-// ## Não sei fazer bonitinho, sorry Leo ## 
-router.get('/produto', function(req, res, next) {
-  res.render('produto', {page:'produto', menuId:'produto'});
-});
 
 // ## Não sei fazer bonitinho, sorry Leo ## 
-router.get('/pedido', function(req, res, next) {
-  res.render('pedido', {page:'pedido', menuId:'pedido'});
+router.get('/produto', function (req, res, next) {
+  res.render('produto', { page: 'produto', menuId: 'produto' });
+});
+
+// ## Não sei fazer bonitinho, sorry Leo ## 
+router.get('/pedido', function (req, res, next) {
+  res.render('pedido', { page: 'pedido', menuId: 'pedido' });
 });
 
 
