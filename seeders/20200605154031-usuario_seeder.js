a6269e06ffcc8d85f8add1eebb1b8bf90bfdeb63
navarroleonardo <abreunavarro.leonardo@gmail.com>
2020-06-05 12:54:46 -0300

    criado seeder usuario

---
+++ seeders/20200605154031-usuario_seeder.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('usuarios', [{
+      nome: 'Bianca',
+      sobrenome: 'Pereira Santos',
+      email: 'bianca@querofesta.com.br',
+      senha: bcrypt.hashSync('123456', 10),
+      cpf: '12312312312',
+      data_nasc: '1995-12-22',
+      sexo: 'F',
+      imagem: null
+    }, {
+      nome: 'Leonardo',
+      sobrenome: 'de Abreu Navarro',
+      email: 'navarro@querofesta.com.br',
+      senha: bcrypt.hashSync('123456', 10),
+      cpf: '43200227850',
+      data_nasc: '2000-03-12',
+      sexo: 'M',
+      imagem: null
+    }, {
+      nome: 'Leonardo',
+      sobrenome: 'Carvalho',
+      email: 'carvalho@querofesta.com.br',
+      senha: bcrypt.hashSync('123456', 10),
+      cpf: '32132132132',
+      data_nasc: '1990-04-01',
+      sexo: 'M',
+      imagem: null
+    }, {
+      nome: 'Melody',
+      sobrenome: 'Gomes',
+      email: 'melody@querofesta.com.br',
+      senha: bcrypt.hashSync('123456', 10),
+      cpf: '21321321321',
+      data_nasc: '1995-08-01',
+      sexo: 'F',
+      imagem: null
+    }]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('usuarios', null, {});
+  }
+};
