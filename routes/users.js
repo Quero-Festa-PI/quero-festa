c98384ba8a22e46c55fe78d5f78c3794a9e7961b
Bianca Santos <bianca.santos37@fatec.sp.gov.br>
2020-06-09 16:00:18 -0300

    rota perfil-vendedor excluida

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,11 @@ router.get('/logar', usuarioController.login);
 router.post('/logar', usuarioController.logar);
 router.get('/cadastro', usuarioController.cadastro);
 router.post('/cadastro', usuarioController.cadastrar);
-router.get('/perfil-vendedor', verificaUsuarioLogado, usuarioController.perfilVendedor);
 router.get('/perfil-cliente', verificaUsuarioLogado, usuarioController.perfilCliente);
 router.get('/editar-cliente/:id', verificaUsuarioLogado, usuarioController.alter);
 router.put('/editar-cliente/:id', verificaUsuarioLogado, usuarioController.update);
 router.get('/dashboard', verificaUsuarioLogado, verificaUsuarioLogado, usuarioController.dashboard);
 router.get('/sair', verificaUsuarioLogado, usuarioController.sair);
-
 
 
 /* GET users listing. */
