589edf852bd7e797e796a887a23d9f27e4e376c9
Bianca Santos <bianca.santos37@fatec.sp.gov.br>
2020-06-22 22:18:43 -0300

    inclusão do multer

--- routes/produto.js
+++ routes/produto.js
@@ -1,5 +1,9 @@
+// Essas duas linhas podem ser resumidas em
+// const router = require('express).Router();
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const upload = multer({dest:'../public/uploads/'});
 
 // Chamando o controller
 const produtoController = require('../controllers/produtoController');
@@ -8,6 +12,7 @@ const produtoController = require('../controllers/produtoController');
 router.get('/produto', produtoController.produto);
 router.get('/buscar', produtoController.buscar);
 router.get('/cadastrar-produto', produtoController.cadastrar);
-// router.post('/cadastrar-produto', produtoController.cadastrar);
+router.post('/cadastrar-produto', upload.any('file', 5), produtoController.cadastro);
+
 
 module.exports = router;
