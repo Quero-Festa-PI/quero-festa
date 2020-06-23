6cdcebf6ad520990a570486ce7f92e0e30c00073
Bianca Santos <bianca.santos37@fatec.sp.gov.br>
2020-06-22 22:17:22 -0300

    cadastro de produto feito

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -1,4 +1,4 @@
-const { sequelize, Sequelize, Produto, AvaliacoesDeProdutos } = require('../database/models')
+const { sequelize, Sequelize, Produto, AvaliacoesDeProdutos, ImagensDeProduto, Loja } = require('../database/models')
 const Op = Sequelize.Op;
 
 module.exports = {
@@ -54,7 +54,50 @@ module.exports = {
         res.render('buscar', { page: 'Resultado da Busca', resultado, search, quantidade, totalPage, pageActual });
     },
     cadastrar: (req, res) => {
-        res.render('cadastrar-produto', { page: 'cadastrar-produto' });
+        let err = req.query.error;
+        if(err == 1){
+            err = 'Digite o nome do produto';
+        }
+        if(err == 2){
+            err = 'informe o preço';
+        }
+        if(err == 4){
+            err = 'Esta disponível para compra?';
+        }          
+        
+        res.render('cadastrar-produto', { page: 'cadastrar-produto', err});
+    },
+    cadastro: async (req, res) => {
+
+        let file = req.files[0].originalname;
+        let img = `/uploads/${file}`;               
+        let {nomeP, preco, descricaoP, disponibilidade} = req.body;
+        
+       
+        if(nomeP.length <= 3){
+            res.redirect('/produtos/cadastrar-produto?error=1')
+        }
+
+        if(preco.length < 3){
+            res.redirect('/produtos/cadastrar-produto?error=2');
+        }
+
+        if(disponibilidade == null){
+            res.redirect('/produtos/cadastrar-produto?error=4');
+        }
+        
+        let produto = await Produto.create({
+            lojas_id: req.session.loja.id,
+            nome: nomeP,
+            valor: preco,
+            descricao: descricaoP,
+            disponibilidade
+        })
+        let imagens = await ImagensDeProduto.create({
+            image_url: img,
+            produtos_id: produto.id
+        })
+        res.redirect(`/loja/${req.session.loja.id}`);
     },
     produto: (req, res) => {
         res.render('produto', { page: 'produto' });
