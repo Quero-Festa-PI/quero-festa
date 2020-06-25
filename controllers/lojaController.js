21fbd399c10b1609c1ac777b2f8d02e3209ffb61
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2020-06-24 23:52:21 -0300

    {feature} add avaliacoes da loja e dos produtos

--- controllers/lojaController.js
+++ controllers/lojaController.js
@@ -1,4 +1,4 @@
-const { sequelize, Usuario, Loja, Produto, ImagensDeProduto} = require('../database/models')
+const { sequelize, Usuario, Loja, Produto, ImagensDeProduto, AvaliacoesDeProdutos } = require('../database/models')
 const bcrypt = require('bcrypt');
 
 module.exports = {
@@ -7,25 +7,49 @@ module.exports = {
           return res.render('index', { page: 'Home' });
      },
      // Pagina da loja
-     loja: async (req, res ) => {
-          let {id} = req.params;
-          let loja = await Loja.findByPk(id,{               
-              include: 
-              [{
-                   model: Produto,
-                   as: 'produtos',
-                   include: [{
-                        model: ImagensDeProduto,
-                        as: 'imagens'
-                    }]    
-               },               
-               {
-                    model: Usuario,
-                    as: 'usuario'
-               }]                           
+     loja: async (req, res) => {
+          let { id } = req.params;
+          let lojaPerfil = await Loja.findByPk(id, {
+               include:
+                    [{
+                         model: Usuario,
+                         as: 'usuario'
+                    }]
           });
 
-          return res.render('perfil-loja', { page: 'Perfil Loja', loja });
+          let avaliacaoLoja = await AvaliacoesDeProdutos.findAll({
+               include: [{
+                    model: Produto,
+                    as: 'produtos',
+                    where: { lojas_id: id },
+               }],
+               attributes: [
+                    [sequelize.fn('avg', sequelize.col('classificacao')), 'media'],
+                    [sequelize.fn('count', sequelize.col('classificacao')), 'quantidade'],
+               ],
+          })
+
+          // buscar produtos no banco de dados
+          let produtos = await Produto.findAll({
+               where: { lojas_id: id },
+               include: [{
+                    model: AvaliacoesDeProdutos,
+                    as: 'avaliacoes',
+                    attributes: [
+                         [sequelize.fn('avg', sequelize.col('classificacao')), 'media'],
+                         [sequelize.fn('count', sequelize.col('classificacao')), 'quantidade']
+                    ],
+               }, {
+                    model: ImagensDeProduto,
+                    as: 'imagens',
+                    attributes: ['image_url'],
+                    limit: 1,
+               }],
+               group: ['avaliacoes.produtos_id'],
+               attributes: ['id', 'nome', 'valor'],
+          });
+
+          return res.render('perfil-loja', { page: 'Perfil Loja', lojaPerfil, resultado: produtos, avaliacaoLoja });
      },
 
      alterarNavegacao: (req, res) => {
