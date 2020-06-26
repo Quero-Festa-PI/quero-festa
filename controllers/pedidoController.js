cbc2fbef63e46f7aeb7bdae607795ce58a767747
Bianca Santos <bianca.santos37@fatec.sp.gov.br>
2020-06-25 22:18:30 -0300

    ajuste quebra da foto: card de dados

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -1,6 +1,39 @@
+const {sequelize, Pedido, Usuario, Loja, Endereco, Pagamento, Entrega, PedidoProduto, Produto} = require('../database/models');
+
 module.exports = {
-    pedido: (req, res) => {
-        res.render('pedido', { page: 'pedido' });
+    pedido: async (req, res) => {
+        let pedido = await Pedido.findOne({
+            include:[{
+                model: Usuario,
+                as: 'usuario',
+                attributes: ['nome', 'sobrenome']
+            },{
+                model: Loja,
+                as: 'loja',
+                attributes: ['nome']
+            },{
+                model: Endereco,
+                as: 'endereco',
+                attributes: ['estado', 'cidade', 'cep', 'logradouro', 'numeral', 'complemento']
+            },{
+                model: Pagamento,
+                as: 'pagamento',
+                attributes: ['forma_pagamento', 'status']
+            },{
+                model: Entrega,
+                as: 'entrega',
+                attributes: ['data_prev', 'data_real']
+            }, {
+                model: PedidoProduto,
+                as: 'listaDeProdutos',
+                attributes: ['pedidos_id', 'produtos_id', 'quantidade']
+            },{
+                model: Produto,
+                as: 'produtos',
+                through: {attributes: []}
+            }]
+        });
+        res.render('pedido', { page: 'pedido', pedido });
     },
     carrinho: (req, res) => {
         res.render('carrinho', { page: 'carrinho' });
